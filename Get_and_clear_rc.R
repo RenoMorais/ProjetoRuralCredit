0443a767c0930a9b9c650736bf96c134687ba2fe
renanmorais1997 <renan.florias@cpiglobal.org>
2023-01-13 16:45:48 -0300

    adicionei novas limpezas e mudanças nas bases para analise e adicionei o download da base geral, que sera utilizada na consulta dos dados agregados, commit na feature 13/01/2022

--- Get_and_clear_rc.R
+++ Get_and_clear_rc.R
@@ -30,33 +30,39 @@ pacman::p_load(tidyverse,
 
 ########################### Directories (variable) ########################################
 root <- paste0("C:/Users/", Sys.getenv("USERNAME"), "/")
-wd_siop_db<- paste0(root,"Dropbox (CPI)/Climate Finance Brazil/01.DATA/Orcamento_Federal/0.Database/")
+wd_workf<- paste0(root,"Dropbox (CPI)/Climate Finance Brazil/01.DATA/BCB/Credito_RuraL_SICOR_RECOR/")
 
 
 ################# Get data with a+pi OLINDA of BCB ##############
 
 #Downlaod subsources: Contratos de Custeio por Município e Produto
+#Date download: 11/01/2023
 cred_bacen <- httr::GET("https://olinda.bcb.gov.br/olinda/servico/SICOR/versao/v2/odata/CusteioMunicipioProduto?$top=3000000&$format=json&$select=Municipio,nomeProduto,AnoEmissao,cdPrograma,cdSubPrograma,cdFonteRecurso,cdTipoSeguro,cdEstado,VlCusteio,cdProduto,codCadMu,Atividade,cdModalidade,codIbge,AreaCusteio")
 
-"date download: 11/01/2023"
-
 "comentar nomes diferentes nas variáveis"
-
-"comentar nome da base na origem"
-
-"variavel qtd não estah disponível provavelmente por sigilo bancário"
 
 "baixar as finalidades restantes do credito rural"
 
 #Downlaod subsources: Contratos de Investimento por Município e Produto
+#Date download: 11/01/2023
 cred_bacen2 <- httr::GET("https://olinda.bcb.gov.br/olinda/servico/SICOR/versao/v2/odata/InvestMunicipioProduto?$format=json&$select=Municipio,nomeProduto,AnoEmissao,cdPrograma,cdSubPrograma,cdFonteRecurso,cdTipoSeguro,cdEstado,VlCusteio,cdProduto,cdMunicipio,Atividade,cdModalidade,AreaInvest")
 
-"date download: 11/01/2023"
+
+#Downlaod subsources: Custeio Investimento Comercial Industrial
+#essa base será utilizada para consultar valores por finalidade ou municipio em relação ao total do credito rural
+#Date download: 13/01/2023
+rc_indus_comerc <- httr::GET("https://olinda.bcb.gov.br/olinda/servico/SICOR/versao/v2/odata/CusteioInvestimentoComercialIndustrialSemFiltros?&$format=json&$select=cdEstado,nomeUF,cdMunicipio,Municipio,AnoEmissao,cdPrograma,cdSubPrograma,cdFonteRecurso,Atividade,QtdCusteio,VlCusteio,QtdInvestimento,VlInvestimento,QtdComercializacao,VlComercializacao,QtdIndustrializacao,VlIndustrializacao,codMunicIbge,AreaCusteio,AreaInvestimento")
+
+
+"OBS: Em ambas as bases não está disponível a variável quantidade,
+provavelmente por sigilo bancário."
 
 #this step is necessary to convert a list into matrix.
 arq_raw <- httr::content(cred_bacen)
 
 arq_raw2 <- httr::content(cred_bacen2)
+
+arq_raw3 <- httr::content(rc_indus_comerc)
 
 
 ################## reducing a list with goal of obtain a oficial list (without description in elements) - necessary if data is a json###############
@@ -64,6 +70,8 @@ arq_raw2 <- httr::content(cred_bacen2)
 data_fact <- arq_raw[[2]]
 
 data_fact2 <- arq_raw2[[2]]
+
+data_fact3 <- arq_raw3[[2]]
 
 length(data_fact)
 
@@ -77,13 +85,13 @@ data_fact <-
 data_fact2 <-
   data.table::rbindlist(data_fact2)
 
-
+data_fact3 <-
+  data.table::rbindlist(data_fact3)
 ############## get data after process of download from api ##############
 
 data_custeio <- readRDS(paste0(wd_workf,"credit.rds"))
 
 data_invest <- readRDS(paste0(wd_workf,"contractbyinvest_clear2.rds"))
-
 
 ################### clear data #####################
 
@@ -93,7 +101,9 @@ data_custeio_clear <- data_custeio %>%
   mutate(municipio=stri_trans_general(str = municipio, id = "Latin-ASCII")) %>% 
   mutate(nome_produto=str_to_lower(nome_produto))%>%
   mutate(nome_produto=stri_trans_general(str = nome_produto, id = "Latin-ASCII")) %>% 
-  mutate(vl_custeio = as.numeric(vl_custeio))#important to add decimal values
+  mutate(vl_custeio = as.numeric(vl_custeio)) %>% #important to add decimal values
+  mutate (value_billion = vl_custeio/1000000000) %>%  #important to visualization into tables
+  rename(cd_municipio = )
 
 data_custeio_clear$nome_produto <- str_replace(data_custeio_clear$nome_produto, '\\"',"") #nao funciona dentro do encadeamento de pipe
 
@@ -103,10 +113,10 @@ data_invest_clear <- data_invest %>%
   mutate(municipio=stri_trans_general(str = municipio, id = "Latin-ASCII")) %>% 
   mutate(nome_produto=str_to_lower(nome_produto))%>%
   mutate(nome_produto=stri_trans_general(str = nome_produto, id = "Latin-ASCII")) %>% 
-  mutate(vl_custeio = as.numeric(vl_custeio))
+  mutate(vl_custeio = as.numeric(vl_custeio)) %>% 
+  mutate (value_billion = vl_custeio/1000000000)
 
 data_invest_clear$nome_produto <- str_replace(data_invest_clear$nome_produto, '\\"',"")
-
 
 ################## compare columns ########
 
@@ -114,9 +124,9 @@ namescolunas_custeio <- as.data.frame(colnames(data_custeio))
 
 namescolunas_invest <- as.data.frame(colnames(data_invest)) 
 
+colnames(data_fact3)
 
-  # rename("colnames(arq_clear)" = "colnames(arq_clear2)")
-
+# rename("colnames(arq_clear)" = "colnames(arq_clear2)")
 
 
 cols_common <- inner_join(namescolunas_invest, namescolunas_custeio)
@@ -139,8 +149,9 @@ saveRDS(data_invest,paste0(wd_workf,"contractbyinvest_clear2.rds"))
 
 saveRDS(data_custeio_clear,paste0(wd_workf,"credit.rds"))
 
-saveRDS(planilha_teste,paste0(wd_workf,"mergedata.rds"))
+saveRDS(data_fact3,paste0(wd_workf,"contract_comerc_indust.rds"))
 
+saveRDS(planilha_teste,paste0(wd_workf,"mergedata.rds"))
 
 
 
